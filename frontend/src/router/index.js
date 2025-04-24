cfd74dd9ea46d374c57f0be1715ba81a9551987c
Wixkun <alexismontoya2004@gmail.com>
2025-04-24 14:14:07 +0200

    changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import Choice from '@/views/Choice.vue';
+import Swipe from '@/views/Swipe.vue';
 import Signin from '@/views/User/Signin.vue';
 import Signout from '@/views/User/Signout.vue';
 import Signup from '@/views/User/Signup.vue';
@@ -12,6 +13,7 @@ const routes = [
     { path : '/signin', name: 'Signin', component: Signin, meta: { title: 'Connexion - MatchRoom', showHeaderFooter: false } },
     { path : '/signout', name: 'Signout', component: Signout, meta: { title: 'Déconnexion - MatchRoom', showHeaderFooter: false } },
     { path : '/signup', name: 'Signup', component: Signup, meta: { title: 'Inscription - MatchRoom', showHeaderFooter: false } },
+    { path : '/swipe/:city', name: 'Swipe', component: Swipe, props: true, meta: { title: 'MatchRoom - :city', showHeaderFooter: true } },
 
 
     { path: '/dashboard', name: 'Dashboard', component: Dashboard, meta: { title: 'Dashboard', showHeaderFooter: false } },
@@ -23,5 +25,15 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
+router.afterEach((to) => {
+    const rawTitle = to.meta.title
+    if (rawTitle) {
+      const city = to.params.city ?? ''
+      document.title = rawTitle.replace(':city', city)
+    } else {
+      document.title = 'MatchRoom'
+    }
+  })
 
 export default router;
